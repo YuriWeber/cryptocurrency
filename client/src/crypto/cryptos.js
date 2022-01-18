f7a0f71ee7e7d208f1be3338e986d8c756de91e7
Yuri Weber <a.yuriprof@gmail.com>
2022-01-18 15:43:25 -0300

    btn text fixed

--- client/src/crypto/cryptos.js
+++ client/src/crypto/cryptos.js
@@ -1,4 +1,5 @@
 // para adicionar um novo botão de crypto basta adicionar o nome e o id (CoinMarketCap) no objeto
 // e a imagem na respectiva pasta
 export const cryptos = {"bitcoin": 1, "litecoin": 2, "xrp": 52, "ethereum": 1027,"cardano": 2010, "cosmos": 3794,
-                    "algorand": 4030, "terra": 4172, "solana": 5426}
+                    "algorand": 4030, "terra": 4172, "solana": 5426, "bnb": 1839, "dogecoin": 74, "polygon": 3890,
+                    "avalanche": 5805, "helium": 5665, "iota": 1720, "tron": 1958}
